5bb8b65c57a2682ae90d7b2b0d0417066aeb6b74
njlr <njlr@users.noreply.github.com>
2017-08-30 09:32:36 +0100

    * Added Checkout message

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,12 +3,16 @@ $ErrorActionPreference = 'Stop';
 $packageName = 'buck';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 
+# We need to ensure that ant is available
+refreshenv
+
 Write-Output "Cloning Buck... "
 cd C:\;
 If (!(Test-Path -Path "buck")) {
   git clone "https://github.com/facebook/buck.git";
 }
 cd buck;
+Write-Output "Checking out tag... "
 $branch = ("tags/v" + $env:ChocolateyPackageVersion);
 Write-Output $branch
 git checkout -q $branch;
